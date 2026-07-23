6021f3374a56a8ae32f2b9df67bfc85ceecb0aad
Whisper25 <whisper252552@gmail.com>
2026-07-23 12:12:36 +0200

    querySelectorAll

--- index.js
+++ index.js
@@ -1,38 +1,30 @@
-const radius = "7px";
-const color = 'green';
-const h1 = document.getElementById("title");
-h1.textContent = "Привіт, DOM!";
-h1.style.textAlign = "center";
-h1.style.color = color;
-const slogan = document.getElementById("slogan");
-slogan.textContent = "Я щойно змінив цей абзац за допомогою JavaScript.";
-slogan.style.textAlign = "center";
-const blocks = document.getElementsByClassName("block");
-for (const block of blocks) {
-  block.style.borderRadius = radius;
-  block.addEventListener('click', ()=>{
-    block.style.backgroundColor='gray';
+// document.getElementById('unique').style.backgroundColor='blue';
+const unique = document.getElementById('unique');
+unique.style.backgroundColor='black';
+unique.style.color='white';
+
+const btns = document.getElementsByClassName('btn');
+for (const btn of btns) {
+  btn.style.display='block';
+  btn.addEventListener('click', ()=>{
+    btn.style.fontSize='32px'
   })
 }
 
-console.log(blocks);
-const articleTitle = document.getElementById("article-title");
-articleTitle.textContent = "Нова назва";
-articleTitle.addEventListener('click', ()=>{
-    articleTitle.style.color=color;
-})
-const image = document.getElementById("image");
-image.src = "./something.jpg";
-image.style.width = "100%";
-image.style.borderRadius = radius;
-
-const year = document.getElementById("year");
-year.textContent = new Date().getFullYear();
-
-const footer = document.getElementById("footer");
-footer.style.textAlign = "center";
-
-image.addEventListener('click', ()=>{
-    image.src = image.src==="http://127.0.0.1:5500/something.jpg" || image.src==='./something.jpg' ? './something-word.webp' : "./something.jpg";
-    console.log(image.src);
-})
+const h2Block = document.querySelector('#block>h2');
+h2Block.style.color='white';
+const btnsBlock = document.querySelectorAll('#block>.btn');
+btnsBlock.forEach(element => {
+  element.style.backgroundColor='yellow';
+});
+for (const btn of btnsBlock) {
+  btn.style.fontSize='16px';
+}
+const olLis = document.querySelectorAll('#block>ol>li');
+olLis.forEach(element => {
+  element.addEventListener('click', ()=>{
+    element.textContent='wow';
+  })
+});
+document.querySelector('#block>ol>li:last-child').style.backgroundColor='pink';
+document.querySelector('#block>ol>li:first-child').style.backgroundColor='pink';
