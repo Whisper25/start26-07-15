03713d777c008b852a3664ff5063d9c475c7e057
Whisper25 <whisper252552@gmail.com>
2026-07-23 16:43:15 +0200

    practice queryselector, addeventlistener, foreach, classlist

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
-const btns = document.querySelectorAll('button');
-for (const btn of btns) {
-btn.classList.add('btn');
-btn.addEventListener('mouseover', ()=>{
-  btn.classList.add('active')
-})
-btn.addEventListener('mouseout', ()=>{
-  btn.classList.remove('active');
-})
-btn.addEventListener('click', ()=>{
-  btn.classList.toggle('shadow');
-})
-}
-const block = document.getElementById('block');
-block.addEventListener('click', ()=>{
-  block.classList.toggle('big-text');
-})
-const clickbtn =document.querySelector('#block>button');
-const picture =document.querySelector('#block>img');
-clickbtn.addEventListener('click',()=>{
-  picture.classList.toggle('hide');
-  clickbtn.textContent=clickbtn.textContent === 'show picture' ? 'hide picture' : 'show picture';
-})
+const countries = new Map();
+countries.set('brasil', 'br');
+countries.set('australia', 'au');
+countries.set('bahamas', 'bs');
+countries.set('USA', 'us');
+countries.set('antarctica', 'aq');
 
-const btnTheme = document.querySelector('button:first-of-type');
-btnTheme.addEventListener('click', ()=>{
-  btnTheme.textContent=btnTheme.textContent==='light'?'dark' : 'light';
-  document.body.classList.toggle('dark');
+const lis = document.querySelectorAll('ul>li');
+const flag = document.querySelector('.flag')
+console.log(lis);
+lis.forEach((li)=>{
+  li.addEventListener('click', ()=>{
+    flag.textContent=li.textContent;
+    flag.style.backgroundImage=`url(https://flagcdn.com/256x192/${countries.get(li.textContent)}.png)`;
+    lis.forEach((li)=>{
+      li.classList.remove('active');
+    })
+    li.classList.add('active');
+  })
 })
